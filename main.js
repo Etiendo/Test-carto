1a133d920863e5cb2542b28c928e4b428d0e9f17
Etiendo <32451923+Etiendo@users.noreply.github.com>
2018-01-12 10:36:35 +0100

    Initialisation

---
+++ main.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZXRpZW5kbyIsImEiOiJjamNhbTY4NDgwOXoxMnptdXIyb2ZvZ2k5In0.zCS4uLuoY-v6bfnDzaEoww';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/etiendo/cjcanntla0vsp2st9m3af6liq',
+  center: [2.33, 48.86],
+  zoom: 10,
+});
